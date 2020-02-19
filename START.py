2e6dd95be493eed9770820a512ee2122510f1823
3dvolt <53141505+3dvolt@users.noreply.github.com>
2020-02-19 15:36:08 +0100

    Update START.py

--- START.py
+++ START.py
@@ -1,19 +1,34 @@
 import os
 from flask import Flask, render_template
+from flask_mysqldb import MySQL,MySQLdb
 
 app = Flask(__name__)
+pp.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'reader'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'Exp'
+mysql = MySQL(app)
 
 @app.route('/')
-def login():
+def index():
       	return render_template('index.html')
 
 @app.route('/fuel')
 def fuel():
         return render_template('gasoline.html')
         
-@app.route('/check')
-def user():
-        return render_template('dashboard.html')
+@app.route('/check',methods=["GET","POST"])
+def logout():
+    if request.method == 'POST':
+        email = request.form['user']
+        password = request.form['psw']
+        curl = mysql.connection.cursor()
+        curl.execute("SELECT * FROM user u WHERE u.username=%s and u.psw =%s",(email,password,))
+        user = curl.fetchone()
+            if len(user) > 1:
+                return render_template('dashboard.html')
+            else
+                return render_template('index.html')
 
 @app.route('/dashboard')
 def index():
