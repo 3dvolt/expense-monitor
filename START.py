b2e07c77f1a2dc08d1c44f1ffcad4ab75be886ea
diego <3dvolt.info@gmail.com>
2020-02-20 00:36:56 +0100

    insert update

--- START.py
+++ START.py
@@ -49,8 +49,8 @@ def benz():
         data = request.form['data']
         kmauto = request.form['totKm']
         mycursor = insert.cursor()
-        mycursor.execute("Insert into fuel values(default,%s ,%s ,%s ,%s )",(costoLitro,costo,kmauto,data,))
-
+        mycursor.execute("Insert into fuel values(default, %s ,%s ,%s ,'%s')",(costoLitro,costo,kmauto,data,))
+        return dashboard()
 
 @app.route('/logout')
 def logout():
